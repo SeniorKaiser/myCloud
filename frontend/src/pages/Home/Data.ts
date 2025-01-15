b196b75b717a96ec09829a6d14648ff7a69b4b59
SeniorKaiser <ilya.dmitrievv2007@gmail.com>
2025-01-15 19:47:51 +0700

    add cards wrapper on home page

---
+++ frontend/src/pages/Home/Data.ts
@@ -0,0 +1,11 @@
+interface FeaturesCards {
+	title: string
+	description: string
+}
+
+const FeaturesCardsList: FeaturesCards[] = [
+	{ title: 'Low Price', description: 'Some info' },
+	{ title: 'Low Price', description: 'Some info' },
+	{ title: 'Low Price', description: 'Some info' },
+	{ title: 'Low Price', description: 'Some info' },
+]
--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Header from '@widgets/Header/Header.tsx'
 import Footer from '@widgets/Footer/Footer.tsx'
 import Welcome from '@widgets/Welcome/Welcome'
+import CardsSlider from '@widgets/CardsSlider/CardsSlider'
 import './Home.css'
 
 const Home: React.FC = () => {
@@ -10,6 +11,14 @@ const Home: React.FC = () => {
 			<Header />
 			<main className='home-main'>
 				<Welcome />
+				<section className='features'>
+					<h1>Features</h1>
+					<CardsSlider
+						width='min(100%, 60rem)'
+						height='100%'
+						cards_in_row={2}
+					/>
+				</section>
 			</main>
 			<Footer />
 		</>
