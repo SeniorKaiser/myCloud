6efb0bafde6ffeca8c17b34fecb6c6d925adb87e
SeniorKaiser <ilya.dmitrievv2007@gmail.com>
2025-01-18 18:24:43 +0700

    add footer

--- frontend/src/widgets/Footer/Footer.tsx
+++ frontend/src/widgets/Footer/Footer.tsx
@@ -1,10 +1,32 @@
 import React from 'react'
 import Logo from '@components/Logo/Logo.tsx'
+import './Footer.css'
 
 const Footer: React.FC = () => {
 	return (
 		<footer>
 			<Logo />
+			<section>
+				<h1>Links</h1>
+				<ul>
+					<li>
+						<a href='./'>Home</a>
+					</li>
+					<li>
+						<a href='./'>About</a>
+					</li>
+					<li>
+						<a href='./'>News</a>
+					</li>
+					<li>
+						<a href='./'>Contacts</a>
+					</li>
+				</ul>
+			</section>
+			<section>
+				<h1>Creators</h1>
+				<p>Ilya Dmitriev</p>
+			</section>
 		</footer>
 	)
 }
