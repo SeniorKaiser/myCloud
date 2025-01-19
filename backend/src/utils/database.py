1bdb04a87dd358da7c870faa4abd8bab44940f9d
SeniorKaiser <ilya.dmitrievv2007@gmail.com>
2025-01-19 19:03:51 +0700

    test

--- backend/src/utils/database.py
+++ backend/src/utils/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite+aiosqlite:///./app.db"
+SQLALCHEMY_DATABASE_URL = "sqlite+aiosqlite:///app.db"
 # SQLALCHEMY_DATABASE_URL = f'postgresql+asyncpg://{settings.DB_USER}:{settings.DB_PASS}@{settings.DB_HOST}:{settings.DB_PORT}/{settings.DB_NAME}'
 
 # Создание подключения
