a457dce56fb86d3ee92b622f3b54dd5f87b12ee9
SeniorKaiser <ilya.dmitrievv2007@gmail.com>
2025-01-19 17:32:03 +0700

    add app.db gitignore

--- frontend/src/pages/Settings/Settings.tsx
+++ frontend/src/pages/Settings/Settings.tsx
@@ -1,17 +1,12 @@
 import React from 'react'
-import Header from '@widgets/Header/Header.tsx'
-import Footer from '@widgets/Footer/Footer.tsx'
-import SizeChanger from '@widgets/SizeChanger/SizeChanger'
+import Header from '@widgets/Header/Header'
 import './Settings.css'
 
 const Settings: React.FC = () => {
 	return (
 		<>
 			<Header />
-			<main>
-				<SizeChanger />
-			</main>
-			<Footer />
+			<main></main>
 		</>
 	)
 }
