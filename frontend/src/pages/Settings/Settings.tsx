fd40c4870457c6b080dca856a7d0aae9669420e1
SeniorKaiser <ilya.dmitrievv2007@gmail.com>
2025-01-24 21:23:51 +0700

    start commit

--- frontend/src/pages/Settings/Settings.tsx
+++ frontend/src/pages/Settings/Settings.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import Header from '@widgets/Header/Header'
-import CardsSlider from '@widgets/CardsSlider/CardsSlider'
 import './Settings.css'
 
 const Settings: React.FC = () => {
