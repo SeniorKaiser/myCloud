234ed8a188b293f3ead0a15cced62bb9c08dd2a6
SeniorKaiser <ilya.dmitrievv2007@gmail.com>
2025-01-25 15:59:26 +0700

    change token dto

--- backend/src/dto/Token.py
+++ backend/src/dto/Token.py
@@ -1,6 +1,6 @@
 from pydantic import BaseModel
 
 class Token(BaseModel):
-    access_token: str
-    refresh_token: str
+    access_token: str | None = None
+    refresh_token: str | None = None
     type: str = "Bearer"
