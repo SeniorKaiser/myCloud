4ed2f4d3cb98d70ad23afa1517d3e94c6440ba3d
SeniorKaiser <ilya.dmitrievv2007@gmail.com>
2025-01-25 18:39:30 +0700

    add request AuthUser frontend

---
+++ frontend/src/services/requests/AuthUser.ts
@@ -0,0 +1,19 @@
+import axios, { AxiosResponse, AxiosError } from 'axios'
+import { domenApi } from '@app/data.ts'
+
+const AuthUser = async (): Promise<string | undefined> => {
+	try {
+		const response: AxiosResponse = await axios.post(
+			`${domenApi}/api/user/auth`,
+			{ withCredentials: true }
+		)
+
+		console.log('User:', response.data)
+	} catch (error) {
+		const axiosError = error as AxiosError
+		console.error('Ошибка:', axiosError.response?.data || axiosError.message)
+		return undefined
+	}
+}
+
+export default AuthUser
