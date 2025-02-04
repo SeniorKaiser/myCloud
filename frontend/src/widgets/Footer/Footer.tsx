b03e04a57f20dd8ec6012acffd458a8d81c2ddd4
SeniorKaiser <ilya.dmitrievv2007@gmail.com>
2025-02-04 21:24:02 +0700

    refactoring footer

--- frontend/src/widgets/Footer/Footer.tsx
+++ frontend/src/widgets/Footer/Footer.tsx
@@ -1,29 +1,16 @@
 import React from 'react'
 import Logo from '@components/Logo/Logo.tsx'
+import Navigation from '@widgets/Navigation/Navigation'
 import './Footer.css'
 
 const Footer: React.FC = () => {
 	return (
 		<footer>
 			<Logo />
-			<section>
-				<h1>Links</h1>
-				<ul>
-					<li>
-						<a href='./'>Home</a>
-					</li>
-					<li>
-						<a href='./'>About</a>
-					</li>
-					<li>
-						<a href='./'>News</a>
-					</li>
-					<li>
-						<a href='./'>Contacts</a>
-					</li>
-				</ul>
+			<section className='footer-nav'>
+				<Navigation />
 			</section>
-			<section>
+			<section className='footer-author'>
 				<h1>Creators</h1>
 				<p>Ilya Dmitriev</p>
 			</section>
