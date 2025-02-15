fa508cc6279b054b94631d131977a9872c30d31d
SeniorKaiser <ilya.dmitrievv2007@gmail.com>
2025-02-15 22:14:04 +0700

    update download

--- frontend/src/services/requests/Download.ts
+++ frontend/src/services/requests/Download.ts
@@ -24,30 +24,11 @@ import { domenApi } from '@app/data'
 
 const downloadFile = async (id: string) => {
 	try {
-		const response = await fetch(`${domenApi}/api/files/download/${id}`, {
-			method: 'GET',
-			headers: {
-				'Content-Type': 'application/json',
-			},
-		})
-
-		if (!response.ok) {
-			throw new Error(`Ошибка загрузки файла: ${response.statusText}`)
-		}
-
-		const blob = await response.blob()
-		const url = URL.createObjectURL(blob)
-
-		// Создаём временную ссылку и эмулируем клик
-		const a = document.createElement('a')
-		a.href = url
-		a.download = `file_${id}` // Можно задать другое имя
-		document.body.appendChild(a)
-		a.click()
-		document.body.removeChild(a)
-
-		// Освобождаем объект URL после скачивания
-		URL.revokeObjectURL(url)
+		const link = document.createElement('a')
+		link.href = `${domenApi}/file/download/${id}`
+		document.body.appendChild(link)
+		link.click()
+		document.body.removeChild(link)
 	} catch (error) {
 		console.error('Ошибка при загрузке файла:', error)
 		alert('Не удалось скачать файл.')
