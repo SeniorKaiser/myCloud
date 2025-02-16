8f67cebf991da5f87a1c5350068cb36bd01870e2
SeniorKaiser <ilya.dmitrievv2007@gmail.com>
2025-02-16 11:35:07 +0700

    remove comments

--- frontend/src/services/requests/Download.ts
+++ frontend/src/services/requests/Download.ts
@@ -1,25 +1,3 @@
-// import axios from 'axios'
-// import { domenApi } from '@app/data'
-
-// const downloadFile = async (fileId: string): Promise<void> => {
-// 	try {
-// 		const response = await axios.get(`${domenApi}/files/download`, {
-// 			params: { file_id: fileId },
-// 			responseType: 'blob',
-// 		})
-// 		const url = window.URL.createObjectURL(new Blob([response.data]))
-// 		const link = document.createElement('a')
-// 		link.href = url
-// 		link.setAttribute('download', 'file')
-// 		document.body.appendChild(link)
-// 		link.click()
-// 		document.body.removeChild(link)
-// 	} catch (error) {
-// 		console.error('Ошибка при загрузке файла', error)
-// 	}
-// }
-
-// export default downloadFile
 import { domenApi } from '@app/data'
 
 const downloadFile = async (id: string) => {
@@ -30,8 +8,7 @@ const downloadFile = async (id: string) => {
 		link.click()
 		document.body.removeChild(link)
 	} catch (error) {
-		console.error('Ошибка при загрузке файла:', error)
-		alert('Не удалось скачать файл.')
+		console.error(error)
 	}
 }
 
