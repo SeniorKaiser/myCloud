368fc552046b53366658dcfbeb09fe9e0192cc08
SeniorKaiser <ilya.dmitrievv2007@gmail.com>
2025-02-18 23:36:16 +0700

    update upload fucntion

--- frontend/src/services/requests/Upload.ts
+++ frontend/src/services/requests/Upload.ts
@@ -4,24 +4,20 @@ import { domenApi } from '@app/data'
 const uploadFile = async (file: File, folderId?: string) => {
 	const formData = new FormData()
 	formData.append('file', file)
-
 	if (folderId) {
 		formData.append('folder_id', folderId)
 	}
+	console.log('Отправляемые данные:')
+	for (let pair of formData.entries()) {
+		console.log(pair[0], pair[1])
+	}
 	try {
-		const response = await axios.post(
-			`${domenApi}/api/files/upload`,
-			formData,
-			{
-				headers: {
-					'Content-Type': 'multipart/form-data',
-				},
-			}
-		)
-		window.location.reload()
+		const response = await axios.post(`${domenApi}/api/files/upload`, formData)
+
+		console.log('Файл успешно загружен:', response.data)
 		return response.data
 	} catch (error) {
-		console.error('Ошибка загрузки файла:', error)
+		console.error(error)
 		throw error
 	}
 }
