46eea2fb81cb6251502bfda5945d2ac48abfc0ce
SeniorKaiser <ilya.dmitrievv2007@gmail.com>
2025-02-23 11:51:49 +0700

    start commit

--- backend/main.py
+++ backend/main.py
@@ -8,7 +8,7 @@ from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI(
     title="myCloud",
     description="Сервис для хранения файлов",
-    version="0.0.3",
+    version="0.0.4",
 )
 
 app.include_router(User_Router)
