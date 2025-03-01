087e1c4b8c91e55a730ca45145246ad13d488a78
SeniorKaiser <ilya.dmitrievv2007@gmail.com>
2025-03-01 18:51:03 +0700

    fix

--- frontend/src/widgets/ReviewsSlider/data.ts
+++ frontend/src/widgets/ReviewsSlider/data.ts
@@ -7,27 +7,27 @@ interface Review {
 
 export const reviewsList: Review[] = [
 	{
-		name: 'Tomas Andre',
+		name: 'Keighley Jarred',
 		stars: 5,
-		text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi earum quod fugit laboriosam doloribus ut impedit harum tenetur temporibus, a non est commodi deleniti adipisci porro quae nihil delectus repellat?',
-		contact: 'test@mail.ru',
+		text: "Excellent service! I've been using it for several months now — everything is working stably. Files download quickly, and most importantly, they can be accessed from any device. Very user-friendly interface!",
+		contact: 'xawopi9390@apklamp.com',
 	},
 	{
-		name: 'Tomas',
+		name: 'Ilene Everett',
 		stars: 4,
-		text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi earum quod fugit laboriosam doloribus ut impedit harum tenetur temporibus, a non est commodi deleniti adipisci porro quae nihil delectus repellat?',
-		contact: 'test@mail.ru',
+		text: "It's a good cloud service, convenient for storing documents and photos. The only negative is that I would like more free space.",
+		contact: 'sipojar218@apklamp.com',
 	},
 	{
-		name: 'Tomas',
+		name: 'Coty Kane',
 		stars: 4,
-		text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi earum quod fugit laboriosam doloribus ut impedit harum tenetur temporibus, a non est commodi deleniti adipisci porro quae nihil delectus repellat?',
-		contact: 'test@mail.ru',
+		text: "Just a top! Finally, I found a service that doesn't slow down, doesn't delete files on its own, and protects data.",
+		contact: 'nociwi4987@arinuse.com',
 	},
 	{
-		name: 'Tomas',
+		name: 'Kilian Dash',
 		stars: 3,
-		text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi earum quod fugit laboriosam doloribus ut impedit harum tenetur temporibus, a non est commodi deleniti adipisci porro quae nihil delectus repellat?',
-		contact: 'test@mail.ru',
+		text: "Overall, I'm happy. Simple setup, intuitive operation, and synchronization is seamless. There was one malfunction, but tech support quickly helped me figure it out. I recommend it!",
+		contact: 'hinabey561@arinuse.com',
 	},
 ]
