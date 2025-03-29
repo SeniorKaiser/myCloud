64a69e4e2e0258b6539c0da7b3eafe44b0eb77dd
SeniorKaiser <ilya.dmitrievv2007@gmail.com>
2025-03-29 18:35:42 +0700

    change starts

--- frontend/src/widgets/ReviewsSlider/data.ts
+++ frontend/src/widgets/ReviewsSlider/data.ts
@@ -7,26 +7,8 @@ interface Review {
 
 export const reviewsList: Review[] = [
 	{
-		name: 'Keighley Jarred',
-		stars: 5,
-		text: "Excellent service! I've been using it for several months now — everything is working stably. Files download quickly, and most importantly, they can be accessed from any device. Very user-friendly interface!",
-		contact: 'xawopi9390@apklamp.com',
-	},
-	{
-		name: 'Ilene Everett',
-		stars: 4,
-		text: "It's a good cloud service, convenient for storing documents and photos. The only negative is that I would like more free space.",
-		contact: 'sipojar218@apklamp.com',
-	},
-	{
-		name: 'Coty Kane',
-		stars: 4,
-		text: "Just a top! Finally, I found a service that doesn't slow down, doesn't delete files on its own, and protects data.",
-		contact: 'nociwi4987@arinuse.com',
-	},
-	{
 		name: 'Нигерийский деятель культуры',
-		stars: 3,
+		stars: 5,
 		text: 'Очень удобное приложение с качественным интерфейсом. Благодаря нему я смог сохранить все самые важные файлы, для которых не хватало места. Рекомендую всем, у кого не хватает памяти на диске.',
 		contact: '',
 	},
